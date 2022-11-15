554216542a5bcada5719f88ba50bbc5b3847b9d8
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-15 17:10:55 +0700

    add login

---
+++ Frontend/src/features/GoogleLogin.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { GoogleLogin } from "react-google-login";
+
+export default function GoogleLogin() {
+  const [loginData, setLoginData] = useState(null);
+
+  const handleSuccess = async (googleData: any) => {
+    console.log(googleData);
+    const res = await fetch("/api/google-auth", {
+      method: "POST",
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    setLoginData(data);
+    localStorage.setItem("loginData", JSON.stringify(data));
+  };
+
+  const handleFailure = (res: any) => {
+    alert(JSON.stringify(res));
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    setLoginData(null);
+  };
+  return <div>GoogleLogin</div>;
+}
