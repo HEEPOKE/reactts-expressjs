86fa8de0a88d796b43f416139d26eb8eb9292543
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-16 16:05:10 +0700

    add yarn

--- Frontend/src/features/GoogleLogin.tsx
+++ Frontend/src/features/GoogleLogin.tsx
@@ -1,11 +1,31 @@
 import { gapi } from "gapi-script";
-import GoogleLogin from "react-google-login";
+import { GoogleLogin } from 'react-google-login';
 
 export default function GoogleLogin() {
+  const clientId =
+    "617246850621-95f9qhmehd380g2df86pjhrqc84n8nij.apps.googleusercontent.com";
+
+  const success = (res: any) => {
+    console.log(res);
+  };
+
+  const error = (res: any) => {
+    console.error(res);
+  };
+
+  const loading = () => {
+    console.log("loading");
+  };
+
   return (
     <>
       <GoogleLogin
-     
+        clientId={clientId}
+        onSuccess={success}
+        onFailure={error}
+        onRequest={loading}
+        responseType="id_token"
+        isSignedIn
       />
     </>
   );
