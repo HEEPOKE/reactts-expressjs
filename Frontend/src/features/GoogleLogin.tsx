e6cd4b242731c937bad9098c194946bc7422c858
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-16 15:16:32 +0700

    del

--- Frontend/src/features/GoogleLogin.tsx
+++ Frontend/src/features/GoogleLogin.tsx
@@ -1,26 +1,12 @@
 import React, { useState } from "react";
+import { gapi } from "gapi-script";
 import { GoogleLogin } from "react-google-login";
-import googleLoginService from "../services/googleLoginService";
 
 export default function GoogleLogin() {
   return (
     <>
       <GoogleLogin
-        clientId={`${process.env.CLIENT_ID}`}
-        buttonText="Login with Google"
-        onSuccess={async (response: any) => {
-          const tokens = await googleLoginService(response);
-          if (!tokens) {
-            alert("Error while logging in w/Google 1 ")
-          } else {
-            // put login logic (i.e. navigating to dashboard page, fetching user from backend
-            // using the new access token, etc
-          }
-        }}
-        onFailure={(response: any) => {
-          alert("Error while logging in w/Google 2")
-        }}
-        cookiePolicy={"single_host_origin"}
+     
       />
     </>
   );
