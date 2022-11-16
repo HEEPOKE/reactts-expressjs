3cf9ad62d38c3fea537d334e51a26048a2278283
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-16 15:38:47 +0700

    Update GoogleLogin.tsx

--- Frontend/src/features/GoogleLogin.tsx
+++ Frontend/src/features/GoogleLogin.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
 import { gapi } from "gapi-script";
-import { GoogleLogin } from "react-google-login";
+import GoogleLogin from "react-google-login";
 
 export default function GoogleLogin() {
   return (
