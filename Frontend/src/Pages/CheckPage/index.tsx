f566997711c352dd85f5c54a2f667f4d85d6fee9
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-17 11:32:34 +0700

    Update index.tsx

--- Frontend/src/Pages/CheckPage/index.tsx
+++ Frontend/src/Pages/CheckPage/index.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import NavbarMenu from "../../components/Navbar";
+import GoogleLoginButton from "../../features/GoogleLoginButton";
 
 export default function CheckPage() {
   return (
-    <div>CheckPAge</div>
-  )
+    <>
+      <NavbarMenu />
+      <GoogleLoginButton />
+    </>
+  );
 }
