eec99682631b96bbaa45673c53893b22532abb9d
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-11-20 04:35:58 +0700

    edit word

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRouter from "./Routes/Approuter";
+import AppRouter from "./Routes/AppRouter";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./index.css";
@@ -8,7 +8,7 @@ import "./assets/css/main.css";
 import "./assets/css/Navbar.css";
 import "./assets/css/Icon.css";
 import "./assets/css/Error.css";
-import "./assets/css/Loading.css"
+import "./assets/css/Loading.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
