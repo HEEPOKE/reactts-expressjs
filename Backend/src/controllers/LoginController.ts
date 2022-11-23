321aef79d8c3116888a9cce04e7cd60c04034435
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-11-23 09:53:22 +0700

    Update LoginController.ts

--- Backend/src/controllers/LoginController.ts
+++ Backend/src/controllers/LoginController.ts
@@ -6,12 +6,28 @@ dotenv.config();
 
 const client = new OAuth2Client(config.CLIENT_ID);
 
+const users: any = [];
+
+function upsert(array: any, item: any) {
+  const i = array.findIndex((_item: any) => _item.email === item.email);
+  if (i > -1) {
+    array[i] = item;
+  } else {
+    array.push(item);
+  }
+}
+
 const loginGoogle = async (req: any, res: any) => {
   const { token } = req.body;
   const ticket = await client.verifyIdToken({
     idToken: token,
     audience: config.CLIENT_ID,
   });
+
+  const { name, email, picture } = ticket.getPayload;
+  upsert(users, [name, email, picture]);
+  res.status(200);
+  res.json({ name, email, picture });
 };
 
 const LoginController = {
